58d2877930359a684416321df90ff3ac4b438075
Scott Fleming <scott.s.fleming@gmail.com>
2019-11-04 19:03:13 -0600

    Sample pa11y.js script configured

--- oc-audit/src/pa11y.js
+++ oc-audit/src/pa11y.js
@@ -1,8 +1,40 @@
 const pa11y = require('pa11y');
 
-pa11y('https://www.commandercu.com/home',{ ignore:['warning','notice'] }).then((results) => {
+let url = 'https://www.omnicommander.com/marketing';
+
+console.log(`Running pa11y on ${url} - Please wait...`);
+
+const options = {
+    ignore: [
+        'warning',
+        'notice'
+    ]
+}
+
+pa11y( url ).then( ( results ) => {
 
     // Do something with the results
-    console.log(results);
+    //  console.log(results);
+
+    let issues = results.issues;
+    let title = results.documentTitle;
+    let url = results.pageUrl;
+    let counter = 1;
+    let rows = issues.length;
+
+    console.log(`Document title: ${title}`);
+    console.log(`URL: ${url}`);
+    console.log(rows + ' errors found:');
+
+    for(issue of issues){
+        console.log(`Item No: ${counter}`);
+        console.log(`Selector: ${issue.selector}`);
+        console.log(`Message: ${issue.message}`);
+        console.log(`Context: ${issue.context}` );
+        
+        counter++;
+        
+    }
+
 
 });
